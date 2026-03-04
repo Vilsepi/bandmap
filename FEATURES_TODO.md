a293f283ac156aef955c6e61898a04195389c1e1
Ville Seppanen <ville@vilsepi.fi>
2026-03-04 19:52:40 +0200

    Add cloudfront distribution

--- FEATURES_TODO.md
+++
@@ -1,2 +0,0 @@
-
-- Add local storage for artists with cache timestamps to lessen queries on backend. Or at the very least add artist name to ratings table. Both recommendations and todo list views currently bombard the backend every time you open the view.
--- packages/infra/src/app.ts
+++ packages/infra/src/app.ts
@@ -9,10 +9,19 @@ if (!lastFmApiKey) {
   throw new Error('Missing env variable: LASTFM_API_KEY. Set it before running cdk deploy.');
 }
 
+const frontendFqdn = process.env['FRONTEND_FQDN'] ?? '';
+const hostedZoneId = process.env['HOSTED_ZONE_ID'] ?? '';
+const hostedZoneName = process.env['HOSTED_ZONE_NAME'] ?? '';
+const frontendCertificateArn = process.env['FRONTEND_CERTIFICATE_ARN'] ?? '';
+
 const _stack = new BandmapStack(app, 'BandmapStack', {
   lastFmApiKey,
+  frontendFqdn,
+  hostedZoneId,
+  hostedZoneName,
+  frontendCertificateArn,
   env: {
-    account: process.env['CDK_DEFAULT_ACCOUNT'],
-    region: process.env['CDK_DEFAULT_REGION'] ?? 'eu-north-1',
+    account: process.env['AWS_ACCOUNT_ID'],
+    region: process.env['AWS_REGION']
   },
 });
